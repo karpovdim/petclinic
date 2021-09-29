fc611898a2d764bb4e729c7d21c7f01a1f781d24
karpovdim <karpov83d@gmail.com>
2021-09-29 14:24:34 +0300

    deleted mistakes in properties , schema.sql , data.sql

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -21,6 +21,10 @@ INSERT INTO types VALUES (3, 'lizard');
 INSERT INTO types VALUES (4, 'snake');
 INSERT INTO types VALUES (5, 'bird');
 INSERT INTO types VALUES (6, 'hamster');
+INSERT INTO types VALUES (7, 'lion');
+INSERT INTO types VALUES (8, 'bear');
+INSERT INTO types VALUES (9, 'dino');
+INSERT INTO types VALUES (10, 'fish');
 
 INSERT INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
 INSERT INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
@@ -47,7 +51,7 @@ INSERT INTO pets VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets VALUES (13, 'Sly', '2012-06-08', 1, 10);
 
-INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
-INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
-INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
+INSERT INTO visits VALUES (1, false, '2013-01-01', 'rabies shot',3,1);
+INSERT INTO visits VALUES (2, false, '2013-01-02', 'rabies shot',2,4);
+INSERT INTO visits VALUES (3, false, '2013-01-03', 'neutered',1,3);
+INSERT INTO visits VALUES (4, false, '2013-01-04', 'spayed',2,5);
